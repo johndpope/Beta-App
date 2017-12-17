cc3c3de7c3fd7fb32e4a352c8ca285709a1f1e99
jamesweber <weber99@purdue.edu>
2017-12-17 18:34:06 -0500

    Visually Working Home Screen

--- Beta Theta Pi/ViewController.swift
+++ Beta Theta Pi/ViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var betaImage: UIImageView!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +23,6 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
 
 }
 
