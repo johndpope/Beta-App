5daeb605f9d59f0cae8643c4766ee8a1188bf523
jamesweber <weber99@purdue.edu>
2017-12-19 00:25:50 -0500

    json File Added

--- Beta Theta Pi/ViewController.swift
+++ Beta Theta Pi/ViewController.swift
@@ -7,14 +7,48 @@
 //
 
 import UIKit
-
 import FacebookLogin
+import FBSDKCoreKit
+import FBSDKLoginKit
 
 class ViewController: UIViewController {
     
     @IBOutlet weak var betaImage: UIImageView!
     @IBOutlet weak var passwordField: UITextField!
     @IBOutlet weak var usernameField: UITextField!
+    
+    /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+    // Code from https://stackoverflow.com/questions/36380389/customized-facebook-login-button-after-integration
+    @IBAction func loginFacebookAction(sender: AnyObject) {//action of the custom button in the storyboard
+        let fbLoginManager : FBSDKLoginManager = FBSDKLoginManager()
+        fbLoginManager.logIn(withReadPermissions: ["email"], from: self) { (result, error) -> Void in
+            if (error == nil){
+                let fbloginresult : FBSDKLoginManagerLoginResult = result!
+                // if user cancel the login
+                if (result?.isCancelled)!{
+                    return
+                }
+                if(fbloginresult.grantedPermissions.contains("email"))
+                {
+                    self.getFBUserData()
+                }
+            }
+        }
+    }
+    
+    func getFBUserData(){
+        if((FBSDKAccessToken.current()) != nil){
+            FBSDKGraphRequest(graphPath: "me", parameters: ["fields": "id, name, first_name, last_name, picture.type(large), email"]).start(completionHandler: { (connection, result, error) -> Void in
+                if (error == nil){
+                    //everything works print the user data
+                    print(result)
+                    print("YAYAY")
+                }
+            })
+        }
+    }
+    /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+    
     
     var username: String = String()
     var password: String = String()
@@ -24,9 +58,6 @@ class ViewController: UIViewController {
         usernameField.delegate = self
         passwordField.delegate = self
         passwordField.isSecureTextEntry = true
-        let loginButton = LoginButton(readPermissions: [ .publicProfile ])
-        loginButton.center = view.center
-        view.addSubview(loginButton)
     }
     
     override func didReceiveMemoryWarning() {
